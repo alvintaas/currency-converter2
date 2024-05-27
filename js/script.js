e9e56d3732c282c691f18f31558108c7469713d7
ALVIN TAAS <alvintaas@gmail.com>
2024-05-27 16:35:58 +0200

    added new files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const dropList = document.querySelectorAll(".drop-list select");
+
+for (let i = 0; i < dropList.length; i++) {
+  for (currency_code in country_code) {
+    console.log(currency_code);
+    // creating option tag with passing currency code as a text and value
+    let optionTag = `<option value="${currency_code}">${currency_code}</option}`;
+    dropList[i].insertAdjacentHTML("beforehand");
+  }
+}
