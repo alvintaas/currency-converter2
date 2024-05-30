587a0bc68bac738bf8fabd8d12c58cc764b701cc
ALVIN TAAS <alvintaas@gmail.com>
2024-05-30 15:45:36 +0200

    bug fixes

--- js/script.js
+++ js/script.js
@@ -2,11 +2,9 @@ const dropList = document.querySelectorAll(".drop-list select");
 
 for (let i = 0; i < dropList.length; i++) {
   for (currency_code in country_code) {
-    console.log(currency_code);
     // creating option tag with passing currency code as a text and value
     let optionTag = `<option value="${currency_code}">${currency_code}</option}`;
-    dropList[i].insertAdjacentHTML("beforehand");
+    //inserting options tag inside select tag
+    dropList[i].insertAdjacentHTML("beforeend", optionTag);
   }
 }
-
-stopped video at 12min 16s
